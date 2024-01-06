50b508af010d58467cc0a24fd26e08a994978f44
vinayak-naik <vinayaknaik2021@gmail.com>
2024-01-06 16:10:43 +0530

    minor

--- config/custom-environment-variables.ts
+++ config/custom-environment-variables.ts
@@ -3,4 +3,6 @@ export default {
   accessTokenPublicKey: "ACCESS_TOKEN_PUBLIC_KEY",
   refreshTokenPrivateKey: "REFRESH_PRIVATE_KEY",
   refreshTokenPublicKey: "REFRESH_PUBLIC_KEY",
+  mongoUri: "MONGO_URI",
+  port: "PORT",
 };
